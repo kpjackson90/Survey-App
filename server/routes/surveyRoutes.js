e077e37536b9c0da572cae0d21394db1d9b70429
Kareem Phillip-Jackson <kpjackson27@gmail.com>
2018-10-24 21:08:34 -0400

    sendgrid implementation

---
+++ server/routes/surveyRoutes.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const requireLogin = require("../middlewares/requireLogin");
+const enoughCredits = require("../middlewares/enoughCredits");
+const Mailer = require("../services/Mailer");
+const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
+
+const Survey = mongoose.model("surveys");
+
+module.exports = app => {
+  app.post("/api/surveys", requireLogin, enoughCredits, (req, res) => {
+    const { title, subject, body, recipients } = req.body;
+
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: recipients.split(",").map(email => ({ email: email.trim() })),
+      _user: req.user.id,
+      dateSent: Date.now()
+    });
+
+    //send email
+    const mailer = new Mailer(survey, surveyTemplate(survey));
+    mailer.send();
+  });
+};
