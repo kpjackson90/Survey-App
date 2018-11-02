cf5a48d7f9f8f6796220bb5487eba8a4de152714
Kareem Phillip-Jackson <kpjackson27@gmail.com>
2018-11-02 15:53:51 -0400

    webhooks complete

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,18 +1,46 @@
 const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const enoughCredits = require("../middlewares/enoughCredits");
+const _ = require("lodash");
+const Path = require("path-parser");
+const { URL } = require("url");
 const Mailer = require("../services/Mailer");
 const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 
 const Survey = mongoose.model("surveys");
 
 module.exports = app => {
-  app.get("/api/surveys/thanks", (req, res) => {
+  app.get("/api/surveys/:surveyId/:choice", (req, res) => {
     res.send("Thanks for voting!");
   });
 
   app.post("/api/surveys/webhooks", (req, res) => {
-    console.log(req.body);
+    const events = _.chain(req.body)
+      .map(({ email, url }) => {
+        const match = p.test(new URL(url).pathname);
+        if (match) {
+          return { email, surveyId: match.surveyId, choice: match.choice };
+        }
+      })
+      .compact()
+      .uniqBy("email", "surveyId")
+      .each(({ surveyId, email, choice }) => {
+        Survey.updateOne(
+          {
+            _id: surveyId,
+            recipients: {
+              $elemMatch: { email: email, responded: false }
+            }
+          },
+          {
+            $inc: { [choice]: 1 },
+            $set: { "recipients.$.responded": true },
+            lastResponded: new Date()
+          }
+        ).exec();
+      })
+      .value();
+
     res.send({});
   });
 
